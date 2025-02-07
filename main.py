976ef763e95c93751f087fcabd5061b8ffabe1a0
X1las <dahepi1@gmail.com>
2025-02-07 10:01:34 +0100

    optimized async and increased range of counting

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ async def main():
     start_time = time.time()
     await count_async()
     async_duration = time.time() - start_time
-    print(f"Async function took {async_duration:.2f} seconds")
 
     start_time = time.time()
     count_sync()
     sync_duration = time.time() - start_time
+    print(f"Async function took {async_duration:.2f} seconds")
     print(f"Sync function took {sync_duration:.2f} seconds")
 
 async def count_async():
-    for i in range(1, 1000001):
+    for i in range(1, 100000001):
         if i % 100000 == 0:
             print(f"Counted to {i} asynchronously")
-        await asyncio.sleep(0)
 
 def count_sync():
-    for i in range(1, 1000001):
+    for i in range(1, 100000001):
         if i % 100000 == 0:
             print(f"Counted to {i} synchronously")
 

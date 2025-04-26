7508517c1ce20ab0878677d412b0d575dd230ded
X1las <dahepix1@gmail.com>
2025-04-26 11:52:04 +0200

    adde csharp changes

---
+++ sync_count_csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class SyncCount
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 1000000000; i++)
+        {
+            if (i % 100000000 == 0)
+            {
+                Console.WriteLine($"Count reached: {i}");
+            }
+        }
+
+        Console.WriteLine("Counting complete!");
+    }
+}

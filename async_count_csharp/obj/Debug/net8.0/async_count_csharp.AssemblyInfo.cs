7b711875a3c9de5378999b050918636630957103
X1las <dahepix1@gmail.com>
2025-05-14 14:03:49 +0200

    adding sources and writing asynchronous chapter

--- async_count_csharp/obj/Debug/net8.0/async_count_csharp.AssemblyInfo.cs
+++ async_count_csharp/obj/Debug/net8.0/async_count_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7306ea29931ac3bcab6fd2aa0627c8373986559")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751deefe772077d13066a99abc93d2a281fc4722")]
 [assembly: System.Reflection.AssemblyProductAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

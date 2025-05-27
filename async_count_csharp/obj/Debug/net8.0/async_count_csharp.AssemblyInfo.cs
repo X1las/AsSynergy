0219f5eed7a4ba182df8f1f12149ffedee47b989
X1las <dahepix1@gmail.com>
2025-05-27 13:49:48 +0200

    updating report

--- async_count_csharp/obj/Debug/net8.0/async_count_csharp.AssemblyInfo.cs
+++ async_count_csharp/obj/Debug/net8.0/async_count_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64679751a9f5d5f6db6f5846a210b2d785a4b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9a3ab03c0f51e2ff559fc7a136399af2bcf1d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("async_count_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
